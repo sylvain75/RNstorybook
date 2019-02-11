4a2af02c758d80210d2fdae4c8a7e7eb7c2e0ddd
Sylvain <sylvoz75@gmail.com>
2019-02-11 18:53:04 +1100

    init

---
+++ App/stories/decorators.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export const BufferView = storyFn => (
+  <View style={{ flex: 1, marginVertical: 40, marginHorizontal: 20 }}>
+    {storyFn()}
+  </View>
+);
+
---
+++ storybook/index.js
@@ -0,0 +1,25 @@
+import { AppRegistry } from 'react-native';
+import { getStorybookUI, configure } from '@storybook/react-native';
+
+import './rn-addons';
+
+// import stories
+configure(() => {
+  require('../App/stories/Button.stories');
+  // require('./stories');
+}, module);
+
+// Refer to https://github.com/storybooks/storybook/tree/master/app/react-native#start-command-parameters
+// To find allowed options for getStorybookUI
+const StorybookUIRoot = getStorybookUI({
+  port: 7007,
+  host: "localhost",
+  onDeviceUI: true,
+  resetStorybook: true
+});
+
+// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
+// If you use Expo you can safely remove this line.
+AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
+
+export default StorybookUIRoot;
