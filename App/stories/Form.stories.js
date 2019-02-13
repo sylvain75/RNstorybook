27ae82a532a6843e4face35027a271f93ef7ef13
Sylvain <sylvoz75@gmail.com>
2019-02-13 11:49:01 +1100

    form stories TextInputs

--- App/stories/Form.stories.js
+++ App/stories/Form.stories.js
@@ -4,25 +4,41 @@ import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
 
 import { BufferView } from './decorators';
-import { Form } from '../components/Form';
+import { Form, FieldWrapper, TextInput } from '../components/Form';
 
-storiesOf('Form', module)
+const defaultTextInputProps = {
+  label: 'Demo',
+  onChangeText: action('onChangeText'),
+};
+storiesOf('Form/TextInput', module)
   .addDecorator(BufferView)
-  .add('default', () => (
-    <Form>
-      <View style={{ flex: 1, backgroundColor: '#e6e6e6' }} />
-    </Form>
+  .add('default', () => <TextInput {...defaultTextInputProps} />)
+  .add('with placeholder', () => (
+    <TextInput {...defaultTextInputProps} placeholder="demo placeholder" />
   ))
-  .add('with header', () => (
-    <Form header="Hello.">
-      <View style={{ flex: 1, backgroundColor: '#e6e6e6' }} />
-    </Form>
+  .add('with value', () => (
+    <TextInput {...defaultTextInputProps} value="hello value" />
   ))
-  .add('with header and subHeader', () => (
-    <Form
-      header="Hello."
-      subHeader="Welcome back. Kindly enter your login details."
-    >
-      <View style={{ flex: 1, backgroundColor: '#e6e6e6' }} />
-    </Form>
+  .add('with error message', () => (
+    <TextInput
+      {...defaultTextInputProps}
+      message="This is an error"
+      messageType="error"
+    />
+  ))
+  .add('email', () => (
+    <TextInput
+      {...defaultTextInputProps}
+      label="Email"
+      value="spencer@reactnativeschool.com"
+      keyboardType="email-address"
+    />
+  ))
+  .add('password', () => (
+    <TextInput
+      {...defaultTextInputProps}
+      label="Password"
+      value="password"
+      secureTextEntry
+    />
   ));
