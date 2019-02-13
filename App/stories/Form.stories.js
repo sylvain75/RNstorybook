d40aa25ccb018d2b997e02305e096058e0e7071c
Sylvain <sylvoz75@gmail.com>
2019-02-13 14:32:36 +1100

    storybook ready

--- App/stories/Form.stories.js
+++ App/stories/Form.stories.js
@@ -4,12 +4,72 @@ import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
 
 import { BufferView } from './decorators';
-import { Form, FieldWrapper, TextInput } from '../components/Form';
+import { Form, FieldWrapper, TextInput, Switch } from '../components/Form';
 
 const defaultTextInputProps = {
   label: 'Demo',
   onChangeText: action('onChangeText'),
 };
+
+storiesOf('Form', module)
+  .addDecorator(BufferView)
+  .add('default', () => (
+    <Form>
+      <View style={{ flex: 1, backgroundColor: '#e6e6e6' }} />
+    </Form>
+  ))
+  .add('with header', () => (
+    <Form header="Hello.">
+      <View style={{ flex: 1, backgroundColor: '#e6e6e6' }} />
+    </Form>
+  ))
+  .add('with header and subheader', () => (
+    <Form
+      header="Hello."
+      subHeader="Welcome back. Kindly enter your login details."
+    >
+      <View style={{ flex: 1, backgroundColor: '#e6e6e6' }} />
+    </Form>
+  ));
+
+storiesOf('Form/FieldWrapper', module)
+  .addDecorator(BufferView)
+  .add('default', () => (
+    <FieldWrapper label="Email">
+      <Text>Hello, wrapper.</Text>
+    </FieldWrapper>
+  ))
+  .add('error message', () => (
+    <FieldWrapper
+      label="Email"
+      message="Please enter a valid email!"
+      messageType="error"
+    >
+      <Text>Hello, wrapper.</Text>
+    </FieldWrapper>
+  ))
+  .add('success message', () => (
+    <FieldWrapper label="Email" message="Looks legit!" messageType="success">
+      <Text>Hello, wrapper.</Text>
+    </FieldWrapper>
+  ))
+  .add('multiple fields', () => (
+    <React.Fragment>
+      <FieldWrapper label="Email">
+        <Text>Hello, wrapper.</Text>
+      </FieldWrapper>
+      <FieldWrapper
+        label="Email"
+        message="Please enter a valid email!"
+        messageType="error"
+      >
+        <Text>Hello, wrapper.</Text>
+      </FieldWrapper>
+      <FieldWrapper label="Email" message="Looks legit!" messageType="success">
+        <Text>Hello, wrapper.</Text>
+      </FieldWrapper>
+    </React.Fragment>
+  ));
 storiesOf('Form/TextInput', module)
   .addDecorator(BufferView)
   .add('default', () => <TextInput {...defaultTextInputProps} />)
@@ -40,5 +100,16 @@ storiesOf('Form/TextInput', module)
       label="Password"
       value="password"
       secureTextEntry
+    />
+  ));
+
+storiesOf('Form/Switch', module)
+  .addDecorator(BufferView)
+  .add('default', () => <Switch label="Agree to Terms" />)
+  .add('with error', () => (
+    <Switch
+      label="Agree to Terms"
+      message="You must agree to the terms"
+      messageType="error"
     />
   ));
